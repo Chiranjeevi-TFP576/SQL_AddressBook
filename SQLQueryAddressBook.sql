01b03ff8235683b1bed8a2aa0cfc7702a7993f3a
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-02-17 08:51:40 +0530

    [chiru] add CreateTable

--- SQLQueryAddressBook.sql
+++ SQLQueryAddressBook.sql
@@ -2,4 +2,16 @@
 --UC1
 create database AddressBook
 use AddressBook
-
+------UC2------
+----CREATING TABLE-----
+CREATE TABLE ADDRESSBOOKSERVICE
+(
+FirstName char(25),
+LastName char(25),
+City char(25),
+StateName char(25),
+Zip int,
+Phone varchar(25),
+Email varchar(25)
+);
+ SELECT * FROM ADDRESS_BOOK_SERVICE;
